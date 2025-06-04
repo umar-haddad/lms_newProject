d48219cace79cfee1d5278de0a7833721cd3d9a7
tiedsandi <fachransandi@gmail.com>
2025-06-04 15:30:09 +0700

    keren

--- lms_angkatan_2.sql
+++ lms_angkatan_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2025 at 10:22 AM
+-- Generation Time: Jun 04, 2025 at 10:29 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -39,6 +39,36 @@ CREATE TABLE `instructors` (
   `update_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `instructors`
+--
+
+INSERT INTO `instructors` (`id`, `name`, `gender`, `education`, `phone`, `email`, `address`, `created_at`, `update_at`) VALUES
+(1, 'Pak agra', 1, 'SMK 100', '085772169466', 'agra12@gmail.com', 'jln.Cipinang Besar Selatan, KAMP.Besar, Cipinang Besar Selatan', '2025-06-04 02:36:21', '2025-06-04 02:55:45'),
+(2, 'bang will', 1, 'Nusa Mandiri', '083342334521', 'william123@gmail.com', 'jln.surabaya Nasgor manchester united', '2025-06-04 07:04:20', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instructors_majors`
+--
+
+CREATE TABLE `instructors_majors` (
+  `id` int(11) NOT NULL,
+  `id_major` int(11) NOT NULL,
+  `id_instructor` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `update_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `instructors_majors`
+--
+
+INSERT INTO `instructors_majors` (`id`, `id_major`, `id_instructor`, `created_at`, `update_at`) VALUES
+(21, 4, 0, '2025-06-04 08:12:09', NULL),
+(24, 3, 17, '2025-06-04 08:14:09', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -47,10 +77,19 @@ CREATE TABLE `instructors` (
 
 CREATE TABLE `majors` (
   `id` int(11) NOT NULL,
-  `name` int(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `update_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `majors`
+--
+
+INSERT INTO `majors` (`id`, `name`, `created_at`, `update_at`) VALUES
+(2, 'Matematika', '2025-06-04 02:35:21', NULL),
+(3, 'web programming', '2025-06-04 06:35:51', NULL),
+(4, 'Design Graphic', '2025-06-04 07:06:19', NULL);
 
 -- --------------------------------------------------------
 
@@ -92,6 +131,13 @@ CREATE TABLE `roles` (
   `update_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `created_at`, `update_at`) VALUES
+(2, 'admin', '2025-06-04 02:31:47', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -114,7 +160,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `update_at`, `deleted_at`) VALUES
 (1, 'umar', 'admin@gmail.com', '12345', '2025-06-03 02:50:21', '2025-06-03 06:45:18', 1),
-(7, 'anjirlah', 'kakkkak@gmail.com', '20202020', '2025-06-03 08:07:04', '2025-06-03 08:07:48', 0);
+(7, 'anjirlah', 'kakkkak@gmail.com', '20202020', '2025-06-03 08:07:04', '2025-06-04 01:29:42', 1),
+(8, 'ajay', 'kkkak12@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964', '2025-06-04 01:42:35', '2025-06-04 01:43:01', 0);
 
 --
 -- Indexes for dumped tables
@@ -124,6 +171,12 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `update_at
 -- Indexes for table `instructors`
 --
 ALTER TABLE `instructors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `instructors_majors`
+--
+ALTER TABLE `instructors_majors`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -164,13 +217,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `instructors`
 --
 ALTER TABLE `instructors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `instructors_majors`
+--
+ALTER TABLE `instructors_majors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `majors`
 --
 ALTER TABLE `majors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `module_details`
@@ -188,13 +247,13 @@ ALTER TABLE `moduls`
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
