8caf4ea21b7d76f9b52c7e377c883c44cd7cbf5c
tiedsandi <fachransandi@gmail.com>
2025-06-11 15:19:13 +0700

    pindahin dlu yang pra_ujikom

--- content/restore-user.php
+++ content/restore-user.php
@@ -1,7 +1,22 @@
 <?php 
-$query = mysqli_query($config, "SELECT * FROM users WHERE deleted_at = 0 ORDER BY id DESC");
+$query = mysqli_query($config, "SELECT * FROM users WHERE deleted_at = 1 ORDER BY id DESC");
 $rows = mysqli_fetch_all($query, MYSQLI_ASSOC);
 
+if(isset($_GET['restore'])) {
+  $restore = $_GET['restore'];
+  $queryRestore = mysqli_query($config, "UPDATE users SET deleted_at = 0 WHERE id=$restore");
+  if ($queryRestore) {
+    header("location: ?page=user&restore=berhasil");
+  } 
+}
+
+if(isset($_GET['delete'])) {
+  $delete = $_GET['delete'];
+  $queryRestore = mysqli_query($config, "DELETE FROM users WHERE id=$delete");
+  if ($queryRestore) {
+    header("location: ?page=user&hapus=berhasil");
+  } 
+}
 ?>
 
 <div class="row">
@@ -10,8 +25,7 @@ $rows = mysqli_fetch_all($query, MYSQLI_ASSOC);
       <div class="card-body">
         <h5 class="card-title"> Data User</h5>
         <div class="mb-3 d-flex justify-content-between">
-          <a href="?page=restore-user" class="btn btn-primary">Restore</a>
-          <a href="?page=tambah-user" class="btn btn-primary">Add User</a>
+          <a href="?page=user" class="btn btn-secondary">Back</a>
         </div>
         <div class="table-responsive">
           <table class="table table-bordered">
@@ -30,9 +44,9 @@ $rows = mysqli_fetch_all($query, MYSQLI_ASSOC);
                 <td><?php echo $row['name'] ?></td>
                 <td><?php echo $row['email'] ?></td>
                 <td>
-                  <a href="?page=tambah-user&edit=<?php echo $row ['id']?>" class="btn btn-primary">Edit</a>
+                  <a href="?page=restore-user&restore=<?php echo $row ['id']?>" class="btn btn-success">Edit</a>
                   <a onclick="return confirm('bener gak mau dihapus?')"
-                    href="?page=tambah-user&delete=<?php echo $row ['id']?>" class="btn btn-danger">Delete</a>
+                    href="?page=restore-user&delete=<?php echo $row ['id']?>" class="btn btn-danger">Delete</a>
                 </td>
               </tr>
               <?php endforeach ?>
--- lms_angkatan_2.sql
+++ lms_angkatan_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 10 Jun 2025 pada 10.39
+-- Waktu pembuatan: 11 Jun 2025 pada 10.17
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `instructors` (
   `id` int(11) NOT NULL,
+  `id_role` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `gender` tinyint(1) NOT NULL,
   `education` varchar(30) NOT NULL,
@@ -44,9 +45,9 @@ CREATE TABLE `instructors` (
 -- Dumping data untuk tabel `instructors`
 --
 
-INSERT INTO `instructors` (`id`, `name`, `gender`, `education`, `phone`, `email`, `password`, `address`, `created_at`, `update_at`) VALUES
-(3, 'rehan', 1, 'SMK 100', '85772169466', 'berak@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964', 'jln.surabaya Nasgor manchester united', '2025-06-05 07:43:09', '2025-06-10 07:53:04'),
-(4, 'Superadmin', 0, 'Nusa Mandiri', '85772169466', 'admin123@gmail.com', '12345', 'jln.surabaya Nasgor manchester united', '2025-06-05 08:05:12', '2025-06-10 07:53:07');
+INSERT INTO `instructors` (`id`, `id_role`, `name`, `gender`, `education`, `phone`, `email`, `password`, `address`, `created_at`, `update_at`) VALUES
+(3, 4, 'rehan', 1, 'SMK 100', '85772169466', 'berak@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'jln.surabaya Nasgor manchester united', '2025-06-05 07:43:09', '2025-06-11 02:29:30'),
+(4, 4, 'naswa', 0, 'Nusa Mandiri', '85772169466', 'naswa@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964', 'jln.surabaya Nasgor manchester united', '2025-06-05 08:05:12', '2025-06-11 01:57:42');
 
 -- --------------------------------------------------------
 
@@ -99,6 +100,36 @@ INSERT INTO `majors` (`id`, `name`, `created_at`, `update_at`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `menus`
+--
+
+CREATE TABLE `menus` (
+  `id` int(11) NOT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `name` varchar(50) NOT NULL,
+  `icon` varchar(50) NOT NULL,
+  `url` varchar(50) DEFAULT NULL,
+  `urutan` int(5) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `update_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `menus`
+--
+
+INSERT INTO `menus` (`id`, `parent_id`, `name`, `icon`, `url`, `urutan`, `created_at`, `update_at`) VALUES
+(1, 0, 'Dashboard', 'bi bi-grid', 'home.php', 1, '2025-06-11 04:21:59', '2025-06-11 05:28:23'),
+(2, 0, 'master data', 'bi bi-menu-button-wide', '', 2, '2025-06-11 04:29:56', '2025-06-11 05:28:24'),
+(5, 2, 'Instructors', 'bi bi-circle', 'instructor', 1, '2025-06-11 05:29:59', '2025-06-11 06:48:43'),
+(6, 2, 'major', 'bi bi-circle', 'major', 2, '2025-06-11 05:29:59', '2025-06-11 06:37:28'),
+(7, 2, 'Menu', 'bi bi-circle', 'menu', 3, '2025-06-11 06:31:03', NULL),
+(8, 2, 'Users', 'bi bi-circle', 'user', 4, '2025-06-11 06:38:50', '2025-06-11 08:10:01'),
+(9, 2, 'Roles', 'bi bi-circle', 'role', 5, '2025-06-11 06:56:42', '2025-06-11 08:08:31');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `module_details`
 --
 
@@ -139,7 +170,6 @@ CREATE TABLE `moduls` (
 --
 
 INSERT INTO `moduls` (`id`, `id_major`, `id_instructor`, `name`, `created_at`, `update_at`) VALUES
-(1, 5, 4, 'cara main sun', '2025-06-10 04:39:49', '2025-06-10 07:56:27'),
 (2, 3, 4, 'pdf', '2025-06-10 06:58:48', '2025-06-10 07:56:31'),
 (3, 3, 4, 'dokumen', '2025-06-10 07:02:12', '2025-06-10 07:56:39');
 
@@ -161,7 +191,11 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `created_at`, `update_at`) VALUES
-(2, 'admin', '2025-06-04 02:31:47', NULL);
+(2, 'admininstrator', '2025-06-04 02:31:47', '2025-06-11 01:21:37'),
+(3, 'admin', '2025-06-11 01:20:57', '2025-06-11 01:21:44'),
+(4, 'instructor', '2025-06-11 01:21:08', NULL),
+(5, 'PIC', '2025-06-11 01:21:16', NULL),
+(6, 'student', '2025-06-11 01:22:03', NULL);
 
 -- --------------------------------------------------------
 
@@ -187,7 +221,7 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `name`, `gender`, `education`, `phone`, `email`, `password`, `address`, `created_at`, `update_at`) VALUES
-(5, 'si william', 0, 'Nusa Mandiri', '85772169466', 'will@gmail.com', '12345', 'jln.surabaya Nasgor manchester united', '2025-06-05 08:05:12', '2025-06-10 07:38:44');
+(5, 'si william', 0, 'Nusa Mandiri', '85772169466', 'will@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964', 'jln.surabaya Nasgor manchester united', '2025-06-05 08:05:12', '2025-06-11 02:39:26');
 
 -- --------------------------------------------------------
 
@@ -210,9 +244,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `update_at`, `deleted_at`) VALUES
-(1, 'umar', 'admin@gmail.com', '12345', '2025-06-03 02:50:21', '2025-06-03 06:45:18', 1),
-(7, 'anjirlah', 'kakkkak@gmail.com', '20202020', '2025-06-03 08:07:04', '2025-06-04 01:29:42', 1),
-(8, 'ajay', 'kkkak12@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964', '2025-06-04 01:42:35', '2025-06-04 01:43:01', 0);
+(1, 'umar', 'admin@gmail.com', '12345', '2025-06-03 02:50:21', '2025-06-11 08:02:35', 0),
+(7, 'anjirlah', 'kakkkak@gmail.com', '20202020', '2025-06-03 08:07:04', '2025-06-11 08:02:41', 0);
 
 --
 -- Indexes for dumped tables
@@ -234,6 +267,12 @@ ALTER TABLE `instructors_majors`
 -- Indeks untuk tabel `majors`
 --
 ALTER TABLE `majors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `menus`
+--
+ALTER TABLE `menus`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -289,6 +328,12 @@ ALTER TABLE `majors`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
+-- AUTO_INCREMENT untuk tabel `menus`
+--
+ALTER TABLE `menus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT untuk tabel `module_details`
 --
 ALTER TABLE `module_details`
@@ -304,7 +349,7 @@ ALTER TABLE `moduls`
 -- AUTO_INCREMENT untuk tabel `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `students`
